45226805f6a6329494ec836a20c52fadb9dd7eaf
elorotron <suselstar2334@mail.ru>
2023-11-08 09:52:00 +0000

    rebuild 1:0

--- dockerfile/files/log_script/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-systemctl daemon-reload
-systemctl enable logs.service
-systemctl start logs.service
-systemctl set-default multi-user.target
-service apache2 start
-nginx -g "daemon off;"
